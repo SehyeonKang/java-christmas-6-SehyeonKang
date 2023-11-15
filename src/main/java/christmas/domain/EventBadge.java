d581037f5a6e73b71a471597e5d984cb42bb76ed
SehyeonKang <gedfgrdt@naver.com>
2023-11-15 12:16:50 +0900

    fix: 이벤트 배지 이름 수정
    총혜택 금액이 5천원 이상일 경우 부여하는 이벤트 배지 이름을 스타->별로 수정

--- src/main/java/christmas/domain/EventBadge.java
+++ src/main/java/christmas/domain/EventBadge.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public enum EventBadge {
     SANTA("산타", 20_000),
     TREE("트리", 10_000),
-    STAR("스타", 5_000),
+    STAR("별", 5_000),
     EMPTY("없음", 0);
 
     private final String name;
